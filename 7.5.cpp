d2fcb62502b6c7a5f3228be1c98528e433a1b1cf
Jeel8758-ai <jeelsavaliya02@gmail.com>
2025-05-09 10:05:30 +0530

    Create 7.5.cpp

---
+++ 7.5.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct Student {
+    string name;
+    int marks;
+    double tuition;
+};
+
+ostream& currency(ostream& os) {
+    os << "Rupees ";
+    return os;
+}
+
+int main() {
+    vector<Student> students = {
+        {"Aanshi", 92, 50000.50},
+        {"Happy", 85, 45500.00},
+        {"Priyal", 78, 47000.75},
+        {"Hani", 88, 52000.25}
+    };
+
+
+    cout << left << setw(15) << "Name"
+         << right << setw(10) << "Marks"
+         << right << setw(20) << "Tuition Fee" << endl;
+
+    cout << setfill('-') << setw(45) << "-" << setfill(' ') << endl;
+
+
+    for (const auto& student : students) {
+        cout << left << setw(15) << student.name
+             << right << setw(10) << student.marks
+             << right << setw(9) << currency << fixed << setprecision(2) << student.tuition << endl;
+    }
+
+    return 0;
+}
